f2dfb121cb616ea95d703265f973c716024a9060
Phillip Igoe <phillipigoe@gmail.com>
2014-11-10 15:20:28 -0500

    fleshing out the process list + simulator class

--- Scheduler/src/ProcessList.java
+++ Scheduler/src/ProcessList.java
@@ -1,4 +1,42 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+/**
+ * Class for the process list that retains all the processes loaded in from the data file
+ * @author phil
+ *
+ */
 public class ProcessList {
-
+	private ArrayList<Process> processes;
+	private int numberProcesses;
+	private int quantum;
+	
+	public ProcessList(String dataFile, int numberOfCyclesToSnapshot) {
+		try {
+			File file = new File(dataFile);		// PI Let's create a file object with a path to the data file
+			FileReader fileReader = new FileReader(file);	// PI create a new file reader
+			BufferedReader bufferedReader = new BufferedReader(fileReader); // PI use a buffered reader to read the file
+			String line; // PI keep a reference to the line
+			int lineNumber = 1; // PI keep track of the line numbers
+			while ((line = bufferedReader.readLine()) != null) {
+				if(lineNumber == 1) { // PI This line has the total # processes
+					String[] parsedLine = line.split("\\s+"); // PI split this line up by spaces
+					numberProcesses = Integer.parseInt(parsedLine[1]); // PI assign the number processes instance field to the value in this line
+				} else if(lineNumber == 2) {
+					String[] parsedLine = line.split("\\s+"); // PI split this line up by spaces
+					quantum = Integer.parseInt(parsedLine[1]); // PI assign the quantum instance field to the value in this line
+				} else if(lineNumber > 3) { // PI otherwise this is just a normal line containing a process
+					String[] parsedLine = line.split("\t"); // PI split this line up by tabs
+				}
+				lineNumber++;
+				
+			}
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Scheduler/src/Simulator.java
+++ Scheduler/src/Simulator.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Simulator {
+	private static ProcessList processList; // PI handle to the ProcessList object
+	
 	/**
 	 * PI the main method is what will be executed when the class is run
 	 * @param args String[] of args passed along to the method
@@ -14,24 +12,12 @@ public class Simulator {
     		// PI parse the args
     		String dataFile = args[0];
         	int numberOfCyclesToSnapshot = Integer.parseInt(args[1]);
+        	processList = new ProcessList(dataFile, numberOfCyclesToSnapshot);
     	} else {
     		System.out.println("Wrong number of command line arguments");
     	}
     } else {
     	System.out.println("You must enter in the name of the datafile + number of cycles to snapshot as an argument!");
     }
-
-		try {
-			File file = new File(dataFile);
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(line);
-			}
-			fileReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
   }
 }
