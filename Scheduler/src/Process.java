c3c548824b40a0e021da25812e8f7c39b53e1d83
fastnick94 <fastnick94@gmail.com>
2014-11-10 15:05:43 -0500

    Add process fields

--- Scheduler/src/Process.java
+++ Scheduler/src/Process.java
@@ -1,4 +1,29 @@
-
 public class Process {
+  private int PID;
+  private int cpuBurst;
+  private int ioBurst;
+  private int period;
 
+  public Process(int PID, int cpuBurst, int ioBurst, int period) {
+    this.PID = PID;
+    this.PID = cpuBurst;
+    this.ioBurst = ioBurst;
+    this.period = period;
+  }
+
+  public int getPID() {
+    return PID;
+  }
+
+  public int getCPUBurst() {
+    return cpuBurst;
+  }
+
+  public int getIOBurst() {
+    return ioBurst;
+  }
+
+  public int getPeriod() {
+    return period;
+  }
 }
