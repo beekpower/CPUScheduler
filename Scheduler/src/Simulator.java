960a5fbe2ee59ad7896f0c811e2f2b7b9f64bf0d
fastnick94 <fastnick94@gmail.com>
2014-11-10 14:48:56 -0500

    Stub to read in files

--- Scheduler/src/Simulator.java
+++ Scheduler/src/Simulator.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Simulator {
 	/**
@@ -5,16 +9,29 @@ public class Simulator {
 	 * @param args String[] of args passed along to the method
 	 */
 	public static void main (String[] args) {
-        if(args.length > 0) { // PI check to see if we were actually passed the name of the data file + # of cycles to snapshot
-        	if(args.length == 2) { // PI check to see if we got the right # of args
-        		// PI parse the args
-        		String dataFile = args[0];
-            	int numberOfCyclesToSnapshot = Integer.parseInt(args[1]);
-        	} else {
-        		System.out.println("Wrong number of command line arguments");
-        	}
-        } else {
-        	System.out.println("You must enter in the name of the datafile + number of cycles to snapshot as an argument!");
-        }
+    if(args.length > 0) { // PI check to see if we were actually passed the name of the data file + # of cycles to snapshot
+    	if(args.length == 2) { // PI check to see if we got the right # of args
+    		// PI parse the args
+    		String dataFile = args[0];
+        	int numberOfCyclesToSnapshot = Integer.parseInt(args[1]);
+    	} else {
+    		System.out.println("Wrong number of command line arguments");
+    	}
+    } else {
+    	System.out.println("You must enter in the name of the datafile + number of cycles to snapshot as an argument!");
     }
+
+		try {
+			File file = new File(dataFile);
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+			}
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+  }
 }
