8e1ed6c0a4139e372f96ec3ec9ffb9425314aaca
Phillip Igoe <phillipigoe@gmail.com>
2014-11-11 17:38:01 -0500

    more tidying up

--- Scheduler/src/Simulator.java
+++ Scheduler/src/Simulator.java
@@ -1,6 +1,7 @@
 
 public class Simulator {
 	private static ProcessList processList; // PI handle to the ProcessList object
+	private static int snapshotInterval; // PI handle to the snapshot interval
 
 	/**
 	 * PI the main method is what will be executed when the class is run
@@ -10,23 +11,18 @@ public class Simulator {
     if(args.length > 0) { // PI check to see if we were actually passed the name of the data file + # of cycles to snapshot
     	if(args.length == 2) { // PI check to see if we got the right # of args
     	  	// PI parse the args
-    		  String dataFile = args[0];
-        	int snapshotInterval = Integer.parseInt(args[1]);
-        	processList = new ProcessList(dataFile, snapshotInterval);
+    		String dataFile = args[0];
+        	snapshotInterval = Integer.parseInt(args[1]); // PI parse the snapshot interval from the CL arg
+        	processList = new ProcessList(dataFile, snapshotInterval); // PI create a new PList
     	} else {
     		System.out.println("Wrong number of command line arguments");
     	}
     } else {
     	System.out.println("You must enter in the name of the datafile + number of cycles to snapshot as an argument!");
     }
-
-		//Create an FCFSScheduler and execute it
-		Scheduler fcfsScheduler = new FCFSScheduler(processList);
-		CPU fcfsCPU = new CPU(fcfsScheduler, snapshotInterval);
-		fcfsCPU.execute();
-
-		
-
-
+	//Create an FCFSScheduler and execute it
+	Scheduler fcfsScheduler = new FCFSScheduler(processList);
+	CPU fcfsCPU = new CPU(fcfsScheduler, snapshotInterval);
+	fcfsCPU.execute();
   }
 }
