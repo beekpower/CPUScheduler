4c6fafb1ffc5da1f93968659e1be5f84188dbf93
Phillip Igoe <phillipigoe@gmail.com>
2014-11-13 20:12:27 -0500

    updates to snapshot class'

--- Scheduler/src/Snapshot.java
+++ Scheduler/src/Snapshot.java
@@ -2,27 +2,29 @@ import java.util.ArrayList;
 
 
 public class Snapshot {
-	public ArrayList<Process> processesInReadyQueue; // PI array list containing the processes in ready queue
-	public ArrayList<Process> processesInIO; // PI array list containing the processes in I/O
+	public Simulator simulator; // PI ref to the simulator
+	public ArrayList<Integer> processesInReadyQueue; // PI array list containing the processes in ready queue
+	public ArrayList<Integer> processesInIO; // PI array list containing the processes in I/O
 	public int numberProcessesCurrentlyProcessing; // PI # processes currently processing
 	public int cycle;	// PI cycle # this was taken at
 	
-	public Snapshot(ArrayList<Process> readyQueue, ArrayList<Process> ioQueue, int numberProcessesCurrentlyProcessing, int cycle) {
+	public Snapshot(Simulator simulator, ArrayList<Process> readyQueue, ArrayList<Process> ioQueue, int numberProcessesCurrentlyProcessing, int cycle) {
 		// PI init array lists
-		processesInReadyQueue = new ArrayList<Process>();
-		processesInIO = new ArrayList<Process>();
+		this.simulator = simulator;
+		processesInReadyQueue = new ArrayList<Integer>();
+		processesInIO = new ArrayList<Integer>();
 		// PI init other stats
 		this.numberProcessesCurrentlyProcessing = numberProcessesCurrentlyProcessing;
 		this.cycle = cycle;
 		
 		// PI copy all the processes in the ready queue to our array list
 		for(Process process: readyQueue) {
-			this.processesInReadyQueue.add(process.copyProcess());
+			this.processesInReadyQueue.add(process.getPID());
 		}
 		
 		// PI copy all the processes in the IO queue to our array list
 		for(Process process: ioQueue) {
-			this.processesInIO.add(process.copyProcess());
+			this.processesInIO.add(process.getPID());
 		}
 	}
 }
