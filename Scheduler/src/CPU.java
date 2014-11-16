66fe23c7b053a1ebfd19dd6324b074356a20e20c
Phillip Igoe <phillipigoe@gmail.com>
2014-11-16 16:50:38 -0500

    gettin the final report set up

--- Scheduler/src/CPU.java
+++ Scheduler/src/CPU.java
@@ -7,11 +7,13 @@ public class CPU {
   int cycleCount = 0; // PI add in a cycle counter
   int snapshotInterval = 0;
   Process currentProcessProcessing;
+  public FinalReport finalReport; // PI final report object
 
   public CPU(Scheduler scheduler, int snapshotInterval) {
     this.scheduler = scheduler;
     this.snapshotInterval = snapshotInterval; // PI set the snapshot interval
     cycleCount = 0; // PI set cycle counter to 0
+    finalReport = new FinalReport(this); // PI make a new final report for this CPU
   }
 
   public void execute() {
@@ -28,6 +30,7 @@ public class CPU {
       if(cycleCount % snapshotInterval == 0) { // PI see if the total # cycles divides evenly w/ the snapshot interval
     	  this.scheduler.processList.takeSnapshot(this); // PI take a snapshot
       }
+      finalReport.addProcess(currentProcessProcessing); // PI add the current process to the final report
       scheduler.schedule();
     }
     System.out.println("\ncomplete execute \n\n");
