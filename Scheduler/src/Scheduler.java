2180140ae15dbd088dc224ebe0ce2197e21d88fc
Phillip Igoe <phillipigoe@gmail.com>
2014-11-18 13:28:11 -0500

    put this back

--- Scheduler/src/Scheduler.java
+++ Scheduler/src/Scheduler.java
@@ -39,6 +39,7 @@ public abstract class Scheduler {
    */
   public void updateCPU(CPU cpu) {
 	  this.cpu = cpu;
+	  currentProcess = getNextProcess();
   }
 
   public abstract Process getNextProcess();
