9bb53486a830c39750f790775594776f0859a48f
Nick <fastnick94@gmail.com>
2014-11-18 13:22:35 -0500

    Rremove getNextProcess from constructor

--- Scheduler/src/Scheduler.java
+++ Scheduler/src/Scheduler.java
@@ -36,7 +36,6 @@ public abstract class Scheduler {
 
   public void updateCPU(CPU cpu) {
 	  this.cpu = cpu;
-	  currentProcess = getNextProcess();
   }
 
   public abstract Process getNextProcess();
