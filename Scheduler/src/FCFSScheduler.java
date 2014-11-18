3768613a1da8c96522d295f3854479a7d9e8f869
Phillip Igoe <phillipigoe@gmail.com>
2014-11-18 13:30:29 -0500

    fix

--- Scheduler/src/FCFSScheduler.java
+++ Scheduler/src/FCFSScheduler.java
@@ -8,8 +8,9 @@ public class FCFSScheduler extends Scheduler{
 
   public Process getNextProcess() {
     if (processList.hasProcessInReadyQueue()) {
-    	cpu.finalReport.addProcess(currentProcess); // PI add the current process to the final report
-    	return processList.takeFirstProcessInReadyQueue();
+    	Process returnProcess = processList.takeFirstProcessInReadyQueue();
+    	cpu.finalReport.addProcess(returnProcess); // PI add the current process to the final report
+    	return returnProcess;
     } else {
       //Set the current process to null. The CPU will see this and enter an idle state
       return null;
