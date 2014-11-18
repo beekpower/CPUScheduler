4254ffc25809aa6a5ddcad347ecd854c71d8b3cf
Nick <fastnick94@gmail.com>
2014-11-18 14:05:19 -0500

    Fix PS loop

--- Scheduler/src/PSScheduler.java
+++ Scheduler/src/PSScheduler.java
@@ -1,19 +1,43 @@
 
 public class PSScheduler extends Scheduler {
 	public PSScheduler(ProcessList processList) {
-	    super(processList);
-	    this.readableName = "PS";
-			preemptive = true;
-	  }
+    super(processList);
+    this.readableName = "PS";
+		preemptive = true;
+	 }
 
-	  public Process getNextProcess() {
-	    if (processList.hasProcessInReadyQueue()) {
-	    	Process returnProcess = processList.takeProcessWithHighestPriority();
-	    	cpu.finalReport.addProcess(returnProcess); // PI add the current process to the final report
-	    	return returnProcess;
-	    } else {
-	      //Set the current process to null. The CPU will see this and enter an idle state
-	      return null;
-	    }
-	  }
+  public Process getNextProcess() {
+
+		if (processList.hasProcessInReadyQueue()) {
+			if (currentProcess == null) {
+				return processWithHighestPriority();
+			} else {
+				if (currentProcess.isTerminated() || currentProcess.isWaiting()) {
+					return processWithHighestPriority();
+				} else if (processList.getProcessWithHighestPriority().getCPUBurst() < currentProcess.getPriority()) {
+					processList.addtoReadyQueue(currentProcess);
+					return processWithHighestPriority();
+				} else {
+					return currentProcess;
+				}
+			}
+		} else {
+			if (currentProcess != null) {
+				if (currentProcess.isTerminated() || currentProcess.isWaiting()) {
+					//current process could still be executing, so return it
+					return null;
+				} else {
+					return currentProcess;
+				}
+			} else {
+				return null;
+			}
+		}
+  }
+
+	private Process processWithHighestPriority() {
+		Process returnProcess = processList.takeProcessWithHighestPriority();
+		cpu.finalReport.addProcess(returnProcess); // PI add the current process to the final report
+		return returnProcess;
+	}
 }
