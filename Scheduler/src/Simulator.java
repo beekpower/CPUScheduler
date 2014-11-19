592626e101a45a9c3be1d4def3860c841a4d6dfe
Phillip Igoe <phillipigoe@gmail.com>
2014-11-18 21:59:15 -0500

    reorder

--- Scheduler/src/Simulator.java
+++ Scheduler/src/Simulator.java
@@ -30,24 +30,24 @@ public class Simulator {
 	Scheduler sjfScheduler = new SJFScheduler(processList);
 	CPU sjfCPU = new CPU(sjfScheduler, snapshotInterval);
 	sjfScheduler.updateCPU(sjfCPU);
-	//sjfCPU.execute();
+	sjfCPU.execute();
+	
+	//Create a RR Scheduler and execute it
+	Scheduler rrScheduler = new RRScheduler(processList);
+	CPU rrCPU = new CPU(rrScheduler, snapshotInterval);
+	rrScheduler.updateCPU(rrCPU);
+	rrCPU.execute();
 
 	//Create an SJF Scheduler and execute it
 	Scheduler sjrScheduler = new SJRScheduler(processList);
 	CPU sjrCPU = new CPU(sjrScheduler, snapshotInterval);
 	sjrScheduler.updateCPU(sjrCPU);
-	//sjrCPU.execute();
+	sjrCPU.execute();
 
 	//Create a PS Scheduler and execute it
 	Scheduler psScheduler = new PSScheduler(processList);
 	CPU psCPU = new CPU(psScheduler, snapshotInterval);
 	psScheduler.updateCPU(psCPU);
 	psCPU.execute();
-
-	//Create a RR Scheduler and execute it
-	Scheduler rrScheduler = new RRScheduler(processList);
-	CPU rrCPU = new CPU(rrScheduler, snapshotInterval);
-	rrScheduler.updateCPU(rrCPU);
-	//rrCPU.execute();
   }
 }
