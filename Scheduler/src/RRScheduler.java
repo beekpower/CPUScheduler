77b3d2558d28f59a7aa31130fc6e54c5d4c90e1f
Phillip Igoe <phillipigoe@gmail.com>
2014-11-20 18:18:12 -0500

    RR

--- Scheduler/src/RRScheduler.java
+++ Scheduler/src/RRScheduler.java
@@ -5,6 +5,42 @@ public class RRScheduler extends Scheduler {
     preemptive = true;
     this.readableName = "RR";
   }
+  
+  @Override
+	public void schedule() {
+	  processList.incrementWaitTimeForProcessesInReadyQueue(); //fix this skip
+	    processList.decrementCurrentProcessesWaiting();
+	    
+	    if (currentProcess != null) {
+	      currentProcess.processInstruction();
+	    }
+
+	    if (processList.hasProcessInReadyQueue()) {
+	        if (currentProcess == null) {
+	      	  currentProcess = takeFirstProcessInReadyQueue();
+	        } else {
+	          if (currentProcess.isTerminated() || currentProcess.isWaiting()) {
+	            currentProcess.resetCounter();
+	            currentProcess =  this.takeFirstProcessInReadyQueue();
+	          } else if (currentProcess.getCounter() == processList.getQuantum()) {
+	            currentProcess.resetCounter();
+	            processList.addtoReadyQueue(currentProcess);
+	            currentProcess = this.takeFirstProcessInReadyQueue();
+	          }
+	        }
+	      }
+	    
+	    else {
+	      if (currentProcess != null) {
+	        if (currentProcess.isTerminated() || currentProcess.isWaiting()) {
+	          //current process could still be executing, so return it
+	          currentProcess = null;
+	        }
+	      } else {
+	        currentProcess = null;
+	      }
+	    }
+	  }
 
   public Process getNextProcess() {
     if (processList.hasProcessInReadyQueue()) {
