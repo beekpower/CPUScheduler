08a530dc274279f3007c1cb697a837e3ed51e58f
Phillip Igoe <phillipigoe@gmail.com>
2014-11-20 20:29:46 -0500

    comparator fix

--- Scheduler/src/Simulator.java
+++ Scheduler/src/Simulator.java
@@ -13,7 +13,7 @@ public class Simulator {
 	 */
 	public static void main (String[] args) {
     if(args.length > 0) { // PI check to see if we were actually passed the name of the data file + # of cycles to snapshot
-    	if(args.length == 1) { // PI check to see if we got the right # of args
+    	if(args.length >= 1) { // PI check to see if we got the right # of args
     	  	// PI parse the args
     		String dataFile = args[0];
     		if(args.length == 2) {
