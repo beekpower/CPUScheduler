99edc94e80a88931e62363562322a1058418e5d0
Phillip Igoe <phillipigoe@gmail.com>
2014-11-20 21:59:35 -0500

    jdoc

--- Scheduler/src/EDFScheduler.java
+++ Scheduler/src/EDFScheduler.java
@@ -5,9 +5,13 @@
  */
 public class EDFScheduler extends Scheduler {
 
+	/**
+	 * NV constructs an EDF scheduler
+	 * @param processList reference to the process list
+	 */
 	public EDFScheduler(ProcessList processList) {
 		super(processList);
-		this.readableName = "EDF";
+		this.readableName = "EDF"; // PI set the readable name
 	}
 
 	@Override
@@ -15,24 +19,24 @@ public class EDFScheduler extends Scheduler {
 		processList.incrementWaitTimeForProcessesInReadyQueue(); // NV increment the wait time for all processes in ready queue
 	    processList.decrementCurrentProcessesWaiting();	// NV decrement the current processes waiting by looping through all processes in IO and decrmenting their IO time
 	    if (currentProcess != null) {
-	      currentProcess.processInstruction(cpu.cycleCount);
+	      currentProcess.processInstruction(cpu.cycleCount); // NV process the next instruction/CPU burst
 	    }
 
 	    if (processList.hasProcessInReadyQueue()) {
 	      if (currentProcess == null) {
-	        currentProcess = processWithSoonestDeadline();
+	        currentProcess = processWithSoonestDeadline(); // NV set current process to process with the soonest deadline
 	      } else {
 	        if (currentProcess.isTerminated() || currentProcess.isWaiting()) {
-	          currentProcess = processWithSoonestDeadline();
+	          currentProcess = processWithSoonestDeadline(); // NV set current process to process with the soonest deadline
 	        } else if (processList.getProcessWithShortestCPUBurst().getCPUBurst() < currentProcess.getCPUBurst()) {
 	          processList.addtoReadyQueue(currentProcess, this.cpu.cycleCount);
-	          currentProcess = processWithSoonestDeadline();
+	          currentProcess = processWithSoonestDeadline(); // NV set current process to process with the soonest deadline
 	        }
 	      }
 	    } else {
 	      if (currentProcess != null) {
 	        if (currentProcess.isTerminated() || currentProcess.isWaiting()) {
-	          //current process could still be executing, so return it
+	          // NV current process could still be executing, so return it
 	          currentProcess = null;
 	        }
 	      } else {
